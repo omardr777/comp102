2bab353e5e2e087b6c7aeccdedd624eabdab481c
rubaian <fa6666n@gmail.com>
2023-05-23 13:42:51 +0300

    fix it

--- pages/FirstPage.py
+++ pages/FirstPage.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+from componets.label import CustomLabel
 class FirstPage(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
         self.controller = controller
         #You can display the components of page1 here 
-        label = tk.Label(self, text="Page 1")
+        label = CustomLabel(self, text="Page 1")
         label.pack(pady=10, padx=10)
             
         button = tk.Button(self, text="Go to Page 2",
