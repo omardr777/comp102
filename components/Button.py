6e29d28c82584bd075e1d1ee75c32d57393a46de
omardr777 <omarcoursee@gmail.com>
2023-05-24 17:59:27 +0300

    button

---
+++ components/Button.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class CustomButton(tk.Button):
+    def __init__(self, master=None, **kwargs):
+        # Customize the appearance
+        kwargs['bg'] = 'blue'  # Set background color
+        kwargs['fg'] = 'white'  # Set foreground color
+        kwargs['font'] = ('Arial', 12, 'bold')  # Set font
+        
+        # Initialize the parent class (Button)
+        super().__init__(master, **kwargs)
