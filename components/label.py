3cb0642446f805cd005cd9f65b0195defc1cecda
terrorbyte704 <hadishamea294@gmail.com>
2023-05-30 19:32:54 +0300

    finished first page layout

--- components/label.py
+++ components/label.py
@@ -1,5 +1,5 @@
 import tkinter as tk
 class CustomLabel(tk.Label):
     def __init__(self, master=None, **kwargs):
-        kwargs['font'] = ('Normal',12,'bold')
+        kwargs['font'] = ('Normal',8,'bold')
         super().__init__(master, **kwargs)
--- pages/FirstPage.py
+++ pages/FirstPage.py
@@ -1,22 +1,36 @@
 import tkinter as tk
 from components.label import CustomLabel
-from components.Checkbox import CheckBox
 from components.Textbox import TextBox
-
+from components.Button import Button
 class FirstPage(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
         self.controller = controller
         #You can display the components of page1 here 
         label = CustomLabel(self, text="Page 1")
-        label.pack(pady=10, padx=10)
-
-        options = ['1','2','3']
-        cardcompoent1 = CheckBox(self,text='click me').pack()
-
-        button = tk.Button(self, text="Go to Page 2",
-                            command=lambda: controller.get_page("SecondPage"))
+        label.place(x=110,y=650)
         
-        testtextbox = TextBox(self)
-        testtextbox.pack()
-        button.pack()
+
+        continue_button = Button(self, text="continue to page 2",
+                            command=lambda: controller.get_page("SecondPage"))
+        continue_button.place(x=50,y=600)
+
+        nametextbox = TextBox(self)
+        nametextbox.place(x=120,y=50)
+
+        nattextbox=TextBox(self)
+        nattextbox.place(x=120,y=100)
+
+        age_text_box=TextBox(self)
+        age_text_box.place(x=120,y=150)
+
+        age_label = CustomLabel(self,text='Enter you age')
+        age_label.place(x=20,y=150)
+
+        name_label = CustomLabel(self, text='Enter your name')
+        name_label.place(x=15, y=50)
+
+        nat_label = CustomLabel(self,text='Enter you Nationality')
+        nat_label.place(x=0,y=100)
+
+        
