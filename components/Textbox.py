0b7f61f090ebc386d1207f99417b359594ebfb5a
omardr777 <omarcoursee@gmail.com>
2023-06-04 01:37:56 +0300

    fixes

--- components/Textbox.py
+++ components/Textbox.py
@@ -3,12 +3,12 @@ import tkinter as tk
 class TextBox(tk.Entry):
     def __init__(self, master=None, **kwargs):
         kwargs['font'] = ('Normal',16)
-        kwargs['borderwidth'] = 0  # Remove the default border
         kwargs['bg']='#F1F5FF'
-        kwargs['highlightcolor'] = '#515486'  # Color of the active border
         kwargs['relief']='solid'
-        kwargs['highlightbackground'] = '#FCFCFC'  # Color of the border
         kwargs['bd']='3'
         kwargs['highlightthickness'] = 0  # Remove the default highlight border
+        kwargs['highlightbackground'] = '#FCFCFC'  # Color of the border
+        kwargs['highlightcolor'] = '#515486'  # Color of the active border
         kwargs['highlightthickness'] = 2  # Border thickness
+        kwargs['borderwidth'] = 0  # Remove the default border
         super().__init__(master, **kwargs)
--- pages/SecondPage.py
+++ pages/SecondPage.py
@@ -55,7 +55,7 @@ class SecondPage(tk.Frame):
         jeddah_checkbox.grid(row=13, column=1)
 
         ChoosingLabel = CustomLabel(self, textvariable=self.userinfo)
-        ChoosingLabel.place(x=500, y=50)
+        ChoosingLabel.grid(row=2, column=2, padx=200)
         
         button = Button(self, text="Search",
                             command=lambda: controller.get_page("LastPage"))
@@ -63,7 +63,4 @@ class SecondPage(tk.Frame):
 
         button = Button(self, text="Go back",
                             command=lambda: controller.get_page("FirstPage"))
-        button.grid(row=13, column=2, padx=900)
-
-    
-   
+        button.grid(row=13, column=3)
