b58cee33e4d71d039f7c0f0a75feb4003b11d258
omardr777 <omarcoursee@gmail.com>
2023-06-03 20:29:47 +0300

    last page

--- components/label.py
+++ components/label.py
@@ -3,4 +3,16 @@ class CustomLabel(tk.Label):
     def __init__(self, master=None, **kwargs):
         kwargs['font'] = ('Normal',16,'bold')
         kwargs['bg'] = '#FCFCFC'
+        kwargs['fg'] = '#515486'
         super().__init__(master, **kwargs)
+    
+    def set_font_size(self,size,weight):
+       fW = weight
+       if fW == '':
+          self.configure(font=(self.cget('font').split()[0],size))
+       else:
+           self.configure(font=(self.cget('font').split()[0],size,fW))
+    
+
+    def set_bg_fg(self,fg,bg):
+       self.configure(bg=bg,fg=fg)
--- pages/LastPage.py
+++ pages/LastPage.py
@@ -10,11 +10,8 @@ class LastPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-
-        # You can display the components of page3 here 
-        label = tk.Label(self, text="Page 3")
-        label.pack(pady=10, padx=10)
-
+        main_color = '#FCFCFC'
+        self['bg'] = main_color
         self.selected_cities_var = tk.StringVar(value=self.controller.selected_cities.get())
 
         def update_selected_cities(*args):
@@ -22,12 +19,41 @@ class LastPage(tk.Frame):
             self.update_selected_cities_labels()
 
         self.controller.selected_cities.trace("w", update_selected_cities)
-
-       
         self.selected_cities_labels = []
         self.selected_city_infos = []
-        button = Button(self, text="Go back", command=self.back_button)
-        button.pack()
+
+        self.grid_columnconfigure(0,weight=1)
+        self.grid_rowconfigure(0,weight=5)
+        self.grid_rowconfigure(1,weight=1)
+        self['bg'] = '#b9bace'
+
+        self.detail_grid = tk.Frame(self)
+        self.detail_grid.grid(row=0,column=0,sticky='news')
+        # self.detail_grid['bg'] = main_color
+        self.detail_grid.grid_columnconfigure(0,weight=1)
+        self.detail_grid.grid_columnconfigure(1,weight=14)
+        self.detail_grid.grid_rowconfigure(0,weight=1)
+
+        self.buttons_frame = tk.Frame(self.detail_grid)
+        self.buttons_frame.grid(row=0,column=0,sticky='nsew')
+        self.buttons_frame['bg'] = '#b9bace'
+        self.config_grid(self.buttons_frame,10,3)
+
+        self.info_frame = tk.Frame(self.detail_grid)
+        self.info_frame.grid_columnconfigure(0,weight=1)
+        self.info_frame.grid_columnconfigure(1,weight=1)
+        self.info_frame.grid_columnconfigure(2,weight=1)
+        self.info_frame.grid_rowconfigure(0,weight=1)
+        self.info_frame.grid_rowconfigure(1,weight=1)
+        self.info_frame.grid_rowconfigure(2,weight=1)
+        self.info_frame.grid_rowconfigure(3,weight=1)
+        self.info_frame.grid_rowconfigure(4,weight=1)
+        self.info_frame.grid(row=0,column=1,sticky='nswe')
+        self.info_frame['bg'] = '#515486'
+
+        back_button = Button(self, text="Go back", command=self.back_button)
+        back_button.grid(row=1,column=0)
+        # back_button.pack()
 
 
     def update_selected_cities_labels(self):
@@ -37,26 +63,66 @@ class LastPage(tk.Frame):
 
         selected_cities = self.selected_cities_var.get().split(',')[1:]
         # here the cities buttons, temporarily will display buttons for each city the user has selected, it should be image buttons
+        counter = 0
         for city in selected_cities:
-            selected_cities_label = Button(self, text=city,command = lambda city=city: self.display_city_info(city))
-            selected_cities_label.pack()
-            self.selected_cities_labels.append(selected_cities_label)
+            print(counter)
+            selected_cities_button = Button(self.buttons_frame, text=city,command = lambda city=city: self.display_city_info(city))
+            selected_cities_button.grid(row=counter,column=1,pady=10)
+            self.selected_cities_labels.append(selected_cities_button)
+            counter += 1
     
-    def display_city_info(self,city_name):
+    def display_city_info(self, city_name):
         print(city_name)
         city_info = self.controller.fetch_city_data(city_name)
-        for item in self.selected_city_infos:
-            item.destroy()
-        self.selected_city_infos.clear()
-        # here the city information, temporarily will display labels,
-        for key,val in city_info.items():
-            city_info_label = CustomLabel(self, text=val)
-            city_info_label.configure(font=(city_info_label.cget('font').split()[0],10,'bold'))
-            city_info_label.pack()
-            self.selected_city_infos.append(city_info_label)
+        print(city_info)
+
+        # Clear any existing labels
+        for label in self.selected_city_infos:
+            label.destroy()
+
+        labels = []
+        rows = [
+            ("City", city_info['city']),
+            ("Description", city_info['description']),
+            ("LandSight to visit", city_info['landSight']),
+            ("Another landSight", city_info['landSight2']),
+            ("LandSight description", city_info['sight1Description']),
+            ("Second landSight description", city_info['sight2Description']),
+            ("Hotel", city_info['hotel']),
+            ("Another hotel", city_info['hotel2']),
+            ("Hotel description", city_info['hotelDescription']),
+            ("Second hotel description", city_info['hotel2Description'])
+        ]
+
+        for i, (row, data) in enumerate(rows):
+            row_label = CustomLabel(self.info_frame, text=row,wraplength=300)
+            row_label.grid(row=i, column=0, sticky='e')
+            row_label.set_font_size(10,'bold')
+            labels.append(row_label)
+
+            data_label = CustomLabel(self.info_frame, text=data,wraplength=1250)
+            data_label.grid(row=i, column=1, sticky='wesn')
+            data_label.set_font_size(10,'')
+            data_label.set_bg_fg('#FCFCFC','#515486')
+            labels.append(data_label)
+
+        # Clear any existing labels
+        for label in self.selected_city_infos:
+            label.destroy()
+
+        self.selected_city_infos = labels
+
+
+
     
     def back_button(self):
         self.controller.get_page('SecondPage')
         for item in self.selected_city_infos:
             item.destroy()
         self.selected_city_infos.clear()
+
+    def config_grid(self,container,rows,columns):
+        for i in range(rows):
+            container.grid_rowconfigure(i,weight=0)
+        for i in range(columns):
+            container.grid_columnconfigure(i,weight=1)
